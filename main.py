3862342e029deba913ceb84f3e5032d1c94cbce1
NikPeg <peganov.nik@gmail.com>
2023-02-16 02:08:29 +0300

    Add all minor buttons

--- main.py
+++ main.py
@@ -21,11 +21,29 @@ def start(message):
 @bot.callback_query_handler(func=lambda call:True)
 def callback_query(call):
     req = call.data.split('_')
+    markup = types.InlineKeyboardMarkup()
     if req[0] == 'info':
-        markup = types.InlineKeyboardMarkup()
         btn1 = types.InlineKeyboardButton(text='Канал проекта', url='https://t.me/scribo_project')
+        btn2 = types.InlineKeyboardButton(text='Главное меню', callback_data='menu')
         markup.add(btn1)
+        markup.add(btn2)
         bot.send_message(call.from_user.id, ABOUT_MESSAGE, reply_markup=markup)
+    elif req[0] == 'menu':
+        btn1 = types.InlineKeyboardButton(text='Загрузить работу', callback_data='download')
+        btn2 = types.InlineKeyboardButton(text='Узнать о Scribo', callback_data='info')
+        btn3 = types.InlineKeyboardButton(text='Связаться с командой', callback_data='connect')
+        btn4 = types.InlineKeyboardButton(text='Отправить донат', url='https://vtb.paymo.ru/collect-money/?transaction'
+                                                                      '=73419948-d0f9-4381-bfa6-93d4bbe35954')
+        markup.add(btn1, btn2)
+        markup.add(btn3, btn4)
+        bot.send_message(call.from_user.id, MENU_MESSAGE, reply_markup=markup)
+    elif req[0] == 'connect':
+        btn1 = types.InlineKeyboardButton(text='Представитель Scribo', url='https://t.me/nikpeg')
+        btn2 = types.InlineKeyboardButton(text='Канал проекта', url='https://t.me/scribo_project')
+        btn3 = types.InlineKeyboardButton(text='Главное меню', callback_data='menu')
+        markup.add(btn1, btn2)
+        markup.add(btn3)
+        bot.send_message(call.from_user.id, CONNECT_MESSAGE, reply_markup=markup)
 
 
 bot.polling(none_stop=True, interval=0)
